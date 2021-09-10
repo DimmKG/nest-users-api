e08c24e970dbda40160b6f67b7d54f001e0da445
DimmKG <dimakrm361@gmail.com>
2021-09-10 22:55:06 +0700

    Remove app controller and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,21 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { ConfigModule} from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm'
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { UserModule } from './user/user.module';
 
-
 @Module({
-  imports: [ConfigModule.forRoot({isGlobal: true}),
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot({
       type: 'postgres',
       url: process.env.DATABASE_URL,
       autoLoadEntities: true,
       synchronize: true,
     }),
-    UserModule],
-  controllers: [AppController],
-  providers: [AppService],
+    UserModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
